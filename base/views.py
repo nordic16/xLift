337f28ad72499cc0dbc803d64f55ec6c17f762cd
nordic16 <diogo6204@gmail.com>
2022-07-31 14:16:42 +0100

    Now 'finish' and discard 'work'

--- base/views.py
+++ base/views.py
@@ -30,33 +30,42 @@ def workout_page_view(request, id):
             sets = ExSet.objects.filter(workout=workout)
             
             return render(request, 'edit_workout.html',
-                context={'form' : form, 'sets' : sets})
+                context={'form' : form, 'sets' : sets, 'id' : id})
 
         else:
             return redirect(reverse_lazy('home'))
         
     else:
         form = AddExerciseForm(request.POST)
-                
         if form.is_valid():
-            data = form.cleaned_data
-            
-            for ex in data["Exercises"]:
-                print(ex.name)
+            if request.POST.get('add-exercise'):    
+                data = form.cleaned_data
                 
-                # If exercise is already in the workout, add another set
-                exset = ExSet.objects.filter(exercise=ex)
-                if exset:
-                    exset[0].number += 1
-                    exset[0].save()
-                    
-                else:
-                    exset = ExSet.objects.create(exercise=ex, reps=0, weight=0, workout=workout)
-                    exset.save()
+                for ex in data["Exercises"]:                
+                    # If exercise is already in the workout, add another set
+                    exset = ExSet.objects.filter(exercise=ex)
+                    if exset:
+                        exset[0].number += 1
+                        exset[0].save()
+                        
+                    else:
+                        exset = ExSet.objects.create(exercise=ex, reps=0, weight=0, workout=workout)
+                        exset.save()
+                
+                return redirect(f'/workouts/{id}')
+
+            # Discards workout and redirects user to home.
+            elif request.POST.get('discard'):
+                workout.delete()
+                return redirect(reverse_lazy('workouts'))
+
+            elif request.POST.get('finish'):
+                print(workout.get_total_volume())
             
-            return redirect(f'/workouts/{id}')
-    
-    
+            
+            return render(request, 'temp.html')
+
+
 def new_workout(request):
     user = request.user
     if request.method == 'GET':    
